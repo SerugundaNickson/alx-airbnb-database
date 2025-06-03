676448445a183ef2667c48041e6ee8f831be0e9b
SerugundaNickson <serugundanickson@gmail.com>
2025-06-03 22:42:08 +0300

    Add subqueries task: non-correlated and correlated queries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,19 @@
+-- Query 1: Properties with average rating > 4.0 using a non-correlated subquery
+SELECT *
+FROM Property
+WHERE id IN (
+    SELECT property_id
+    FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+-- Query 2: Correlated subquery to find users who have made more than 3 bookings
+SELECT *
+FROM User
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking
+    WHERE Booking.user_id = User.id
+) > 3;
 
